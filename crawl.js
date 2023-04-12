f38eab07cb64bb169c1057204c2c6b03bb92a4ad
Gord L <rndedg@gmail.com>
2023-04-11 20:04:12 -0700

    Created function to check arguments, and set the baseURL.

--- crawl.js
+++ crawl.js
@@ -1,6 +1,8 @@
+const { JSDOM } = require('jsdom');
+
 function normalizeURL(url) {
   const urlObj = new URL(url);
-  let fullPath = `${irlObj.host}${urlObj.pathname}`;
+  let fullPath = `${urlObj.host}${urlObj.pathname}`;
 
   if (fullPath.length > 0 && fullPath.slice(-1) === '/'){
     fullPath = fullPath.slice(0, -1);
@@ -8,4 +10,27 @@ function normalizeURL(url) {
   return fullPath;
 };
 
-module.exports = { normalizeURL };
+function getURLsFromHTML(htmlBody, baseURL){
+  const urls = [];
+  const dom = new JSDOM(htmlBody);
+  let aTags = dom.window.document.querySelectorAll('a');
+
+  for (const aTag of aTags){
+    if (aTag.href.slice(0,1) === '/'){
+      try {
+        urls.push(new URL(aTag.href, baseURL).href)
+      } catch (err){
+        console.log(`${err.message}: ${aTag.href}`)
+      }
+    } else {
+      try {
+        urls.push(new URL(aTag.href).href)
+      } catch (err){
+        console.log(`${err.message}: ${aTag.href}`)
+      }
+    }
+  }
+  return urls;
+};
+
+module.exports = { normalizeURL, getURLsFromHTML };
--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-console.log("Hello, world");
+function main(){
+  if(process.argv.length < 3){
+    console.log("No website provided.");
+  } else if (process.argv.length > 3){
+    console.log("Too many arguments provided.");
+  } else {
+    const baseURL = process.argv[2];
+    console.log(`Excellent, starting crawl at ${baseURL}`);
+  }
+};
+
+main();
