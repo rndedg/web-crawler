7f09353fda55d8c4ee3cb46dc05d5c500891d744
Gord L <rndedg@gmail.com>
2023-04-11 20:22:10 -0700

    Created crawlPage function to fetch data from baseURL.

--- crawl.js
+++ crawl.js
@@ -1,36 +1,56 @@
-const { JSDOM } = require('jsdom');
+const { JSDOM } = require("jsdom");
+const fetch = require("node-fetch");
 
 function normalizeURL(url) {
   const urlObj = new URL(url);
   let fullPath = `${urlObj.host}${urlObj.pathname}`;
 
-  if (fullPath.length > 0 && fullPath.slice(-1) === '/'){
+  if (fullPath.length > 0 && fullPath.slice(-1) === "/") {
     fullPath = fullPath.slice(0, -1);
   }
   return fullPath;
-};
+}
 
-function getURLsFromHTML(htmlBody, baseURL){
+function getURLsFromHTML(htmlBody, baseURL) {
   const urls = [];
   const dom = new JSDOM(htmlBody);
-  let aTags = dom.window.document.querySelectorAll('a');
+  let aTags = dom.window.document.querySelectorAll("a");
 
-  for (const aTag of aTags){
-    if (aTag.href.slice(0,1) === '/'){
+  for (const aTag of aTags) {
+    if (aTag.href.slice(0, 1) === "/") {
       try {
-        urls.push(new URL(aTag.href, baseURL).href)
-      } catch (err){
-        console.log(`${err.message}: ${aTag.href}`)
+        urls.push(new URL(aTag.href, baseURL).href);
+      } catch (err) {
+        console.log(`${err.message}: ${aTag.href}`);
       }
     } else {
       try {
-        urls.push(new URL(aTag.href).href)
-      } catch (err){
-        console.log(`${err.message}: ${aTag.href}`)
+        urls.push(new URL(aTag.href).href);
+      } catch (err) {
+        console.log(`${err.message}: ${aTag.href}`);
       }
     }
   }
   return urls;
-};
+}
 
-module.exports = { normalizeURL, getURLsFromHTML };
+async function crawlPage(currentURL) {
+  console.log(`Crawling ${currentURL}`)
+  try {
+    const response = await fetch(currentURL);
+    if (response.status > 399) {
+      console.log(`Got an error, status code: ${response.status}`);
+      return;
+    }
+    const contentType = response.headers.get("content-type");
+    if (!contentType.includes("text/html")) {
+      console.log(`Got a non-html response: ${contentType}`);
+      return;
+    }
+    console.log(await response.text());
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+
+module.exports = { normalizeURL, getURLsFromHTML, crawlPage };
--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+const { crawlPage } = require("./crawl");
+
+
 function main(){
   if(process.argv.length < 3){
     console.log("No website provided.");
@@ -6,6 +9,7 @@ function main(){
   } else {
     const baseURL = process.argv[2];
     console.log(`Excellent, starting crawl at ${baseURL}`);
+    crawlPage(baseURL);
   }
 };
 
